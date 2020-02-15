9cab3ba264ec7541182abb9f90bac6d394fe0aca
Noam Chapnik <chapnik13@gmail.com>
2020-02-15 18:21:59 +0200

    Initial Commit

---
+++ TextEnrichment/Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Hosting;
+using System.Threading.Tasks;
+
+namespace TextEnrichment
+{
+    public static class Program
+    {
+        public static async Task Main()
+        {
+            var builder = new HostBuilder();
+
+            await builder.RunConsoleAsync().ConfigureAwait(false);
+        }
+    }
+}
